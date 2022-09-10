6a4229ccca173e012c6042f9a6e34467f9048a13
Kenneth V. Domingo <hello@kvdomingo.xyz>
2022-09-10 17:45:44 +0800

    cleanup driver after scrape

--- sbs_image_scraper/__main__.py
+++ sbs_image_scraper/__main__.py
@@ -8,7 +8,9 @@ from loguru import logger
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
+from selenium.webdriver.support.expected_conditions import (
+    presence_of_all_elements_located,
+)
 from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
 
@@ -16,12 +18,14 @@ from tqdm import tqdm
 def main(board_number: int):
     options = Options()
     options.headless = True
+    options.log = False
     driver = Firefox(options=options)
     driver.get(f"https://programs.sbs.co.kr/enter/gayo/visualboard/54795?cmd=view&page=1&board_no={board_number}")
     try:
         WebDriverWait(driver, 10).until(presence_of_all_elements_located((By.CLASS_NAME, "aba_img")))
     finally:
         html = driver.page_source
+        driver.close()
     soup = BeautifulSoup(html, "lxml")
     images = soup.find_all(attrs={"class": "aba_img"})
     sources = [img.get("src") for img in images]
